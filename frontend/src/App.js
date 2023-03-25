8e0b9c6809cf3f827e6fe1542bb1da2f2e972771
Tejaswikarthik <tejaswikarthiknistala@gmail.com>
2023-03-25 13:33:37 +0530

    create-react-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">pasfik</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
