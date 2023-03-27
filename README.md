fd47759c0f0405af4b4e621b111bde1f9038ea68
Tejaswikarthik <tejaswikarthiknistala@gmail.com>
2023-03-27 17:28:42 +0530

    backend

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # MERN-STACK PROJECT
 
 # CONTENTS
+
 1. installing tools
 2. react-App
 3. github push
 4. List Products
-    1. create product list array
-    2. add product images
-    3. render products
-    4. Style products
+   1. create product list array
+   2. add product images
+   3. render products
+   4. Style products.
+5. Routing
+   1. npm i react-router-dom
+   2. create route for homescreen
+   3. create route for product screen.
+
+6. Backend
+   1. npm init
+   2. updating json file.
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">pasfik</a>
-      </header>
-      <main>list products</main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">pasfik</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
